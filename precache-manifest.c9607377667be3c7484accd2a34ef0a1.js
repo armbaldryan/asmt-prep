f3f2fcc7ffe70eea952f9a50ce9eecbd61e0225f
mms/Armen_Baldryan <Baldryan_Armen@gmail.com>
2020-10-16 19:46:03 +0400

    Updates

---
+++ precache-manifest.c9607377667be3c7484accd2a34ef0a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6100487f232e8373d30f9656d4b2acb5",
+    "url": "/asmt-prep/index.html"
+  },
+  {
+    "revision": "1383988911bc37fbdda8",
+    "url": "/asmt-prep/static/js/2.02988aa8.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/asmt-prep/static/js/2.02988aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e60535c5aa2b1823a5",
+    "url": "/asmt-prep/static/js/main.8d97f782.chunk.js"
+  },
+  {
+    "revision": "a3b739121db4ca676e0e",
+    "url": "/asmt-prep/static/js/runtime-main.695ac4f2.js"
+  },
+  {
+    "revision": "3f14c3000381901d9615440cb94045de",
+    "url": "/asmt-prep/static/media/bg.3f14c300.png"
+  },
+  {
+    "revision": "30e89ef9fcd6f50da76626d8a5b691fc",
+    "url": "/asmt-prep/static/media/ci-cd.30e89ef9.png"
+  },
+  {
+    "revision": "464784e308c723cf20dfcd832a345378",
+    "url": "/asmt-prep/static/media/logo.464784e3.PNG"
+  },
+  {
+    "revision": "e50a044ffc1ff85fbefa6d81f52563c2",
+    "url": "/asmt-prep/static/media/welcome_header.e50a044f.svg"
+  }
+]);
