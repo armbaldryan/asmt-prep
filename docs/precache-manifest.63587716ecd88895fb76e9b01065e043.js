23c76a570aaae90b24780fde98ad464d46913ba1
mms/Armen_Baldryan <Baldryan_Armen@gmail.com>
2020-10-16 19:37:33 +0400

    Adding build

---
+++ docs/precache-manifest.63587716ecd88895fb76e9b01065e043.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887582ae724391ab9d4b0f6bfebbec69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c03af9929eb71796994c",
+    "url": "/static/js/2.b07296f6.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/static/js/2.b07296f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e60535c5aa2b1823a5",
+    "url": "/static/js/main.8d97f782.chunk.js"
+  },
+  {
+    "revision": "5f0825cc90e82ab9a1e1",
+    "url": "/static/js/runtime-main.bc025eb2.js"
+  },
+  {
+    "revision": "3f14c3000381901d9615440cb94045de",
+    "url": "/static/media/bg.3f14c300.png"
+  },
+  {
+    "revision": "30e89ef9fcd6f50da76626d8a5b691fc",
+    "url": "/static/media/ci-cd.30e89ef9.png"
+  },
+  {
+    "revision": "464784e308c723cf20dfcd832a345378",
+    "url": "/static/media/logo.464784e3.PNG"
+  },
+  {
+    "revision": "e50a044ffc1ff85fbefa6d81f52563c2",
+    "url": "/static/media/welcome_header.e50a044f.svg"
+  }
+]);
